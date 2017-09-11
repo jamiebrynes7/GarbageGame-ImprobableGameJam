93b43679f1015aa1bf2a1602f28ff9371bf3db0b
Jessica Falk <jessica@jessica-mbp.internal.improbableworlds.net>
2017-09-11 16:12:39 +0100

    adding bin bag collision

---
+++ workers/unity/Assets/Gamelogic/Player/Behaviours/BinBagDie.cs
@@ -0,0 +1,28 @@
+﻿using Improbable.Unity;
+using Improbable.Unity.Visualizer;
+using UnityEngine;
+using Improbable.Player;
+
+namespace Assets.Gamelogic.Pirates.Behaviours
+{
+	[WorkerType(WorkerPlatform.UnityWorker)]
+	public class BinBagDie : MonoBehaviour
+	{
+		[Require] private BinbagInfo.Writer BinbagInfoWriter;
+
+		private void OnTriggerEnter(Collider other)
+		{
+			if (BinbagInfoWriter == null)
+				return;
+
+			if (BinbagInfoWriter.Data.health <= 0)
+				return;
+			
+			if (other != null && other.gameObject.tag == "binman")
+			{
+				uint newHealth = BinbagInfoWriter.Data.health - 1;
+				BinbagInfoWriter.Send (new BinbagInfo.Update().SetHealth(newHealth));
+			}
+		}
+	}
+}
---
+++ workers/unity/Assets/Gamelogic/Player/Behaviours/SpawnBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using Improbable.Player;
+using Improbable.Unity;
+using Improbable.Unity.Visualizer;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Assets.Gamelogic.Player.Behaviours
+{
+	// Add this MonoBehaviour on client workers only
+	[WorkerType(WorkerPlatform.UnityClient)]
+	public class SpawnBehaviour : MonoBehaviour
+	{
+		// Inject access to the entity's Health component
+		[Require] private BinbagInfo.Reader binbagInfoReader;
+
+		public Animation SinkingAnimation;
+		public GameObject ShipWashVfx;
+
+		private bool alreadySunk = false;
+
+		private void OnEnable()
+		{
+			alreadySunk = false;
+			binbagInfoReader.HealthUpdated.Add(OnCurrentHealthUpdated);
+		}
+
+		private void OnDisable()
+		{
+			binbagInfoReader.HealthUpdated.Remove(OnCurrentHealthUpdated);
+		}
+
+		// Callback for whenever the CurrentHealth property of the Health component is updated
+		private void OnCurrentHealthUpdated(uint currentHealth)
+		{
+			if (currentHealth <= 0)
+			{
+				Respawn ();
+			}
+		}
+
+		private void Respawn()
+		{
+			Vector3 position = new Vector3(Random.Range(-200,200), 0, Random.Range(-200, 200));
+			NavMeshHit hit;
+			NavMesh.SamplePosition(position, out hit, 10, 1);
+			position = hit.position;
+			this.transform.position = position;
+		}
+	}
+}
