100e947e49812a3e8bde5af0dc584a3c80b5721e
Jessica Falk <jessica@jessica-mbp.internal.improbableworlds.net>
2017-09-13 12:59:24 +0100

    added spawning of stones with pressing Q

---
+++ workers/unity/Assets/Gamelogic/Player/Behaviours/BinmanInteractions.cs
@@ -0,0 +1,27 @@
+﻿using Improbable.Unity;
+using Improbable.Unity.Core;
+using Improbable.Unity.Visualizer;
+using UnityEngine;
+using Improbable.Player;
+using Improbable.Unity;
+using Improbable.Unity.Core;
+using Improbable;
+using Improbable.Core;
+using Improbable.Environment;
+using Improbable.Unity.Common.Core.Math;
+
+
+[WorkerType(WorkerPlatform.UnityClient)]
+public class BinmanInteractions : MonoBehaviour
+{
+	[Require] StoneInfo.Writer StoneInfoWriter;
+
+	private void Update(){
+		if (Input.GetKeyDown(KeyCode.Q))
+		{
+			Debug.Log ("Dropping Stone");
+			StoneInfoWriter.Send (new StoneInfo.Update().AddSpawn(new SpawnData(this.transform.position.ToSpatialCoordinates())));
+		}
+	}
+}
+
