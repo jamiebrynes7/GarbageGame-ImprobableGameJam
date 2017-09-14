9111a8f0205d0ec21f24135fa588ae5e1e4afe33
Jamie Brynes <jamiebrynes@improbable.io>
2017-09-14 10:51:38 +0100

    feat: Health bar

---
+++ workers/unity/Assets/Gamelogic/Player/Behaviours/HealthGuiController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Improbable.Unity;
+using Improbable.Unity.Visualizer;
+
+using Improbable.Player;
+using Improbable.Core;
+
+[WorkerType(WorkerPlatform.UnityClient)]
+public class HealthGuiController : MonoBehaviour {
+
+	private Slider slider;
+
+	[Require] private BinbagInfo.Reader BinbagInfoReader;
+	[Require] private ClientAuthorityCheck.Writer ClientAuthorityCheckWriter;
+
+	private void OnEnable()
+	{
+		BinbagInfoReader.HealthUpdated.Add(OnHealthUpdated);
+		GameObject c = GameObject.Find("Canvas");
+		GameObject sliderContainer = c.transform.Find("HealthDisplay").gameObject;
+		sliderContainer.SetActive(true);
+		slider = sliderContainer.GetComponent<Slider>();
+	}
+
+	private void OnDisable()
+	{
+		BinbagInfoReader.HealthUpdated.Add(OnHealthUpdated);
+	}
+
+	private void OnHealthUpdated(int health)
+	{
+		float normValue = Mathf.Clamp01(health/10f);
+		if (slider != null)
+		{
+			slider.normalizedValue = normValue;
+		}
+	}
+}
